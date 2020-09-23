fbc804c3ed303437403e392918847955e1a5f4de
shazena <shazena93@gmail.com>
2020-09-23 16:49:23 -0400

    Adding test class

---
+++ SampleProject/src/main/java/com/skshazena/sampleproject/testing/TestClassExample.java
@@ -0,0 +1,12 @@
+
+package com.skshazena.sampleproject.testing;
+
+/**
+ *
+ * @author Shazena Khan 
+ * 
+Date Created: Sep 23, 2020
+ */
+public class TestClassExample {
+    //this is the test class example
+}
