8dd6d1aca8f116cd457081a26f075b37150cd85d
Kristina Zakharova <zakharova.kr@gmail.com>
2020-09-26 11:17:54 -0400

    added power and superhero DTOs

--- SuperheroSightings/src/main/java/com/skkzas/superherosightings/dto/Power.java
+++ SuperheroSightings/src/main/java/com/skkzas/superherosightings/dto/Power.java
@@ -1,5 +1,7 @@
 package com.skkzas.superherosightings.dto;
 
+import java.util.Objects;
+
 /**
  *
  * @author Shazena Khan, Kristina Zakharova, Arfin Shah
@@ -7,4 +9,58 @@ package com.skkzas.superherosightings.dto;
  * Date Created: Sep 25, 2020
  */
 public class Power {
+
+    private int powerId;
+    private String powerName;
+
+    public int getPowerId() {
+        return powerId;
+    }
+
+    public void setPowerId(int powerId) {
+        this.powerId = powerId;
+    }
+
+    public String getPowerName() {
+        return powerName;
+    }
+
+    public void setPowerName(String powerName) {
+        this.powerName = powerName;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + this.powerId;
+        hash = 97 * hash + Objects.hashCode(this.powerName);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Power other = (Power) obj;
+        if (this.powerId != other.powerId) {
+            return false;
+        }
+        if (!Objects.equals(this.powerName, other.powerName)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Power{" + "powerId=" + powerId + ", powerName=" + powerName + '}';
+    }
+
 }
--- SuperheroSightings/src/main/java/com/skkzas/superherosightings/dto/Superhero.java
+++ SuperheroSightings/src/main/java/com/skkzas/superherosightings/dto/Superhero.java
@@ -1,5 +1,7 @@
 package com.skkzas.superherosightings.dto;
 
+import java.util.Objects;
+
 /**
  *
  * @author Shazena Khan, Kristina Zakharova, Arfin Shah
@@ -7,4 +9,84 @@ package com.skkzas.superherosightings.dto;
  * Date Created: Sep 25, 2020
  */
 public class Superhero {
+
+    private int superheroId;
+    private String superheroName;
+    private String superheroDescription;
+    private Power power;
+
+    public int getSuperheroId() {
+        return superheroId;
+    }
+
+    public void setSuperheroId(int superheroId) {
+        this.superheroId = superheroId;
+    }
+
+    public String getSuperheroName() {
+        return superheroName;
+    }
+
+    public void setSuperheroName(String superheroName) {
+        this.superheroName = superheroName;
+    }
+
+    public String getSuperheroDescription() {
+        return superheroDescription;
+    }
+
+    public void setSuperheroDescription(String superheroDescription) {
+        this.superheroDescription = superheroDescription;
+    }
+
+    public Power getPower() {
+        return power;
+    }
+
+    public void setPower(Power power) {
+        this.power = power;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 13 * hash + this.superheroId;
+        hash = 13 * hash + Objects.hashCode(this.superheroName);
+        hash = 13 * hash + Objects.hashCode(this.superheroDescription);
+        hash = 13 * hash + Objects.hashCode(this.power);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Superhero other = (Superhero) obj;
+        if (this.superheroId != other.superheroId) {
+            return false;
+        }
+        if (!Objects.equals(this.superheroName, other.superheroName)) {
+            return false;
+        }
+        if (!Objects.equals(this.superheroDescription, other.superheroDescription)) {
+            return false;
+        }
+        if (!Objects.equals(this.power, other.power)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Superhero{" + "superheroId=" + superheroId + ", superheroName=" + superheroName + ", superheroDescription=" + superheroDescription + ", power=" + power + '}';
+    }
+    
 }
