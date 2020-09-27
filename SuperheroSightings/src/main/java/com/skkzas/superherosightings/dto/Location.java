d5011cc03be953689e4db2a24c5b87b565a12a21
shazena <shazena93@gmail.com>
2020-09-27 16:25:00 -0400

    Updating Location and Sighting DTO

--- SuperheroSightings/src/main/java/com/skkzas/superherosightings/dto/Location.java
+++ SuperheroSightings/src/main/java/com/skkzas/superherosightings/dto/Location.java
@@ -1,5 +1,7 @@
 package com.skkzas.superherosightings.dto;
 
+import java.util.Objects;
+
 /**
  *
  * @author Shazena Khan, Kristina Zakharova, Arfin Shah
@@ -7,4 +9,136 @@ package com.skkzas.superherosightings.dto;
  * Date Created: Sep 25, 2020
  */
 public class Location {
+
+    private int locationId;
+    private String locationName;
+    private String description;
+    private String address;
+    private String city;
+    private String zip;
+    private String latitude;
+    private String longitude;
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + this.locationId;
+        hash = 17 * hash + Objects.hashCode(this.locationName);
+        hash = 17 * hash + Objects.hashCode(this.description);
+        hash = 17 * hash + Objects.hashCode(this.address);
+        hash = 17 * hash + Objects.hashCode(this.city);
+        hash = 17 * hash + Objects.hashCode(this.zip);
+        hash = 17 * hash + Objects.hashCode(this.latitude);
+        hash = 17 * hash + Objects.hashCode(this.longitude);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (this.locationId != other.locationId) {
+            return false;
+        }
+        if (!Objects.equals(this.locationName, other.locationName)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.address, other.address)) {
+            return false;
+        }
+        if (!Objects.equals(this.city, other.city)) {
+            return false;
+        }
+        if (!Objects.equals(this.zip, other.zip)) {
+            return false;
+        }
+        if (!Objects.equals(this.latitude, other.latitude)) {
+            return false;
+        }
+        if (!Objects.equals(this.longitude, other.longitude)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "locationId=" + locationId + ", locationName=" + locationName + ", description=" + description + ", address=" + address + ", city=" + city + ", zip=" + zip + ", latitude=" + latitude + ", longitude=" + longitude + '}';
+    }
+
 }
