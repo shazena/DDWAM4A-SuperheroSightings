599aacb87197ce2ec4d24cc688a85c6555bbaf29
shazena <62306982+shazena@users.noreply.github.com>
2020-10-01 02:57:48 -0400

    Create SightingDaoDBTest.java

---
+++ SuperheroSightings/src/test/java/com/skkzas/superherosightings/dao/SightingDaoDBTest.java
@@ -0,0 +1,77 @@
+package com.skkzas.superherosightings.dao;
+
+import com.skkzas.superherosightings.dto.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *
+ * @author Shazena Khan, Kristina Zakharova, Arfin Shah
+ *
+ * Date Created: Sep 30, 2020
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SightingDaoDBTest {
+
+    public SightingDaoDBTest() {
+    }
+
+    @BeforeEach
+    public static void setUpClass() {
+    }
+
+    /**
+     * Test of getSightingById method, of class SightingDaoDB.
+     */
+    @Test
+    public void testGetSightingById() {
+    }
+
+    /**
+     * Test of getAllSightings method, of class SightingDaoDB.
+     */
+    @Test
+    public void testGetAllSightings() {
+    }
+
+    /**
+     * Test of getLastTenSightings method, of class SightingDaoDB.
+     */
+    @Test
+    public void testGetLastTenSightings() {
+    }
+
+    /**
+     * Test of addSighting method, of class SightingDaoDB.
+     */
+    @Test
+    public void testAddSighting() {
+    }
+
+    /**
+     * Test of updateSighting method, of class SightingDaoDB.
+     */
+    @Test
+    public void testUpdateSighting() {
+    }
+
+    /**
+     * Test of deleteSightingById method, of class SightingDaoDB.
+     */
+    @Test
+    public void testDeleteSightingById() {
+    }
+
+    /**
+     * Test of getAllSightingsForDate method, of class SightingDaoDB.
+     */
+    @Test
+    public void testGetAllSightingsForDate() {
+    }
+
+}
