67ed7367e00efa9b7a99ec1e6ee694ca0b7906ae
Kristina Zakharova <zakharova.kr@gmail.com>
2020-10-06 14:40:35 -0400

    working on add and edit for superhero

--- SuperheroSightings/src/main/java/com/skkzas/superherosightings/controllers/SuperheroController.java
+++ SuperheroSightings/src/main/java/com/skkzas/superherosightings/controllers/SuperheroController.java
@@ -1,12 +1,15 @@
 package com.skkzas.superherosightings.controllers;
 
 import com.skkzas.superherosightings.dao.*;
+import com.skkzas.superherosightings.dto.Power;
 import com.skkzas.superherosightings.dto.Superhero;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -35,10 +38,27 @@ public class SuperheroController {
     @GetMapping("superheroes")
     public String displayAllSuperheroes(Model model) {
         List<Superhero> allSuperheroes = superheroDao.getAllSuperheros();
+        List<Power> powers = powerDao.getAllPowers();
 
         model.addAttribute("allSuperheroes", allSuperheroes);
+        model.addAttribute("powers", powers);
 
         return "superheroes";
+    }
+
+    @PostMapping("addSuperhero")
+    public String addSuperhero(Superhero superhero, HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String powerId = request.getParameter("powerId");
+        String description = request.getParameter("superheroDescription");
+
+        superhero.setSuperheroName(name);
+        superhero.setPower(powerDao.getPowerById(Integer.parseInt(powerId)));
+        superhero.setSuperheroDescription(description);
+
+        superheroDao.addSuperhero(superhero);
+
+        return "redirect:/superheroes";
     }
 
     @GetMapping("superheroDetails")
@@ -48,5 +68,40 @@ public class SuperheroController {
         return "superheroDetails";
     }
 
+    @GetMapping("superheroEdit")
+    public String editSuperhero(Integer id, Model model) {
+        Superhero superhero = superheroDao.getSuperheroById(id);
+        List<Power> powers = powerDao.getAllPowers();
+        model.addAttribute("superhero", superhero);
+        model.addAttribute("powers", powers);
 
+        return "superheroEdit";
+    }
+
+    @PostMapping("superheroEdit")
+    public String performSuperheroEdit(HttpServletRequest request) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Superhero superhero = superheroDao.getSuperheroById(id);
+
+        String name = request.getParameter("name");
+        String powerId = request.getParameter("powerId");
+        String description = request.getParameter("description");
+
+        superhero.setSuperheroName(name);
+        superhero.setPower(powerDao.getPowerById(Integer.parseInt(powerId)));
+        superhero.setSuperheroDescription(description);
+
+        superheroDao.updateSuperhero(superhero);
+
+        return "redirect:/superheroes";
+    }
+
+//    int id = Integer.parseInt(request.getParameter("id"));
+//    Power power = powerDao.getPowerById(id);
+//
+//        power.setPowerName(request.getParameter("name"));
+//
+//        powerDao.updatePower(power);
+//
+//        return "redirect:/powers";
 }
