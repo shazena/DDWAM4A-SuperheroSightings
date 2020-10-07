781e20fd391b102095395f0997f5f8e0fa39e490
shazena <62306982+shazena@users.noreply.github.com>
2020-10-07 01:05:04 -0400

    Location Can now be Added, Except for Latitude and Longitude

--- SuperheroSightings/src/main/java/com/skkzas/superherosightings/controllers/LocationController.java
+++ SuperheroSightings/src/main/java/com/skkzas/superherosightings/controllers/LocationController.java
@@ -7,10 +7,12 @@ import com.skkzas.superherosightings.dao.SightingDao;
 import com.skkzas.superherosightings.dao.SuperheroDao;
 import com.skkzas.superherosightings.dto.Location;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -45,10 +47,36 @@ public class LocationController {
         return "locations";
     }
 
+    @PostMapping("addLocation")
+    public String addLocation(HttpServletRequest request) {
+
+        String locationName = request.getParameter("locationName");
+        String address = request.getParameter("address");
+        String city = request.getParameter("city");
+        String state = request.getParameter("state");
+        String zip = request.getParameter("zip");
+        String description = request.getParameter("description");
+
+        Location location = new Location();
+        location.setLocationName(locationName);
+        location.setAddress(address);
+        location.setCity(city);
+        location.setState(state);
+        location.setZip(zip);
+        location.setDescription(description);
+        location.setLatitude("4.3");
+        location.setLongitude("4.3");
+
+        //need to figure out how to call the API and get the coordinates!!!
+        locationDao.addLocation(location);
+        return "redirect:/locations";
+    }
+
     @GetMapping("locationDetails")
     public String locationDetails(Integer id, Model model) {
         Location theLocation = locationDao.getLocationById(id);
         model.addAttribute("location", theLocation);
+        //get the map to show up on this page too!!!
         return "locationDetails";
     }
 
