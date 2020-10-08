30299438e72310d6fae1ef78b31ee0380f088894
shazena <62306982+shazena@users.noreply.github.com>
2020-10-07 20:13:24 -0400

    Sightings can be viewed on the page, don't like the date format

--- SuperheroSightings/src/main/java/com/skkzas/superherosightings/controllers/SightingController.java
+++ SuperheroSightings/src/main/java/com/skkzas/superherosightings/controllers/SightingController.java
@@ -1,6 +1,17 @@
 package com.skkzas.superherosightings.controllers;
 
+import com.skkzas.superherosightings.dao.LocationDao;
+import com.skkzas.superherosightings.dao.OrganizationDao;
+import com.skkzas.superherosightings.dao.PowerDao;
+import com.skkzas.superherosightings.dao.SightingDao;
+import com.skkzas.superherosightings.dao.SuperheroDao;
+import com.skkzas.superherosightings.dto.Location;
+import com.skkzas.superherosightings.dto.Sighting;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  *
@@ -10,4 +21,30 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class SightingController {
+
+    @Autowired
+    PowerDao powerDao;
+
+    @Autowired
+    SuperheroDao superheroDao;
+
+    @Autowired
+    OrganizationDao organizationDao;
+
+    @Autowired
+    LocationDao locationDao;
+
+    @Autowired
+    SightingDao sightingDao;
+
+    @GetMapping("sightings")
+    public String displayAllSightings(Model model) {
+
+        List<Sighting> allSightings = sightingDao.getAllSightings();
+
+        model.addAttribute("allSightings", allSightings);
+
+        return "sightings";
+    }
+
 }
