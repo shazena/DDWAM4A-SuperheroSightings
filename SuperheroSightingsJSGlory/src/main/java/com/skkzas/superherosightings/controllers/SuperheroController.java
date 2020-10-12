cc787d82288362b844b782f205de6c638515a3f9
shazena <62306982+shazena@users.noreply.github.com>
2020-10-12 05:21:54 -0400

    Superhero can now be validated in the backend with all the fancy front end JS as well!!!

--- SuperheroSightingsJSGlory/src/main/java/com/skkzas/superherosightings/controllers/SuperheroController.java
+++ SuperheroSightingsJSGlory/src/main/java/com/skkzas/superherosightings/controllers/SuperheroController.java
@@ -16,7 +16,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -46,6 +51,9 @@ public class SuperheroController {
     @Autowired
     ImageDao imageDao;
 
+    Set<ConstraintViolation<Superhero>> violations = new HashSet<>();
+    Set<ConstraintViolation<Superhero>> violationsEdit = new HashSet<>();
+
     private final String SUPERHERO_UPLOAD_DIRECTORY = "Superheroes";
 
     @GetMapping("superheroes")
@@ -55,12 +63,14 @@ public class SuperheroController {
 
         model.addAttribute("allSuperheroes", allSuperheroes);
         model.addAttribute("powers", powers);
+        violations.clear();
+        model.addAttribute("errors", violations);
 
         return "superheroes";
     }
 
     @PostMapping("addSuperhero")
-    public String addSuperhero(HttpServletRequest request, @RequestParam("file") MultipartFile file) {
+    public String addSuperhero(Model model, HttpServletRequest request, @RequestParam("file") MultipartFile file) {
         String fileLocation = imageDao.saveImage(file, Long.toString(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)), SUPERHERO_UPLOAD_DIRECTORY);
 
         String name = request.getParameter("name");
@@ -75,7 +85,6 @@ public class SuperheroController {
         } else {
             String powerName = request.getParameter("powerName");
             power.setPowerName(powerName);
-            power = powerDao.addPower(power);
         }
 
         superhero.setSuperheroName(name);
@@ -83,9 +92,24 @@ public class SuperheroController {
         superhero.setSuperheroDescription(description);
         superhero.setPhotoFileName(fileLocation);
 
-        superheroDao.addSuperhero(superhero);
+        Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
+        violations = validate.validate(superhero);
 
-        return "redirect:/superheroes";
+        if (violations.isEmpty()) {
+            if (superhero.getPower().getPowerId() == 0) {
+                power = powerDao.addPower(power);
+            }
+            superheroDao.addSuperhero(superhero);
+            return "redirect:/superheroes";
+        } else {
+            List<Superhero> allSuperheroes = superheroDao.getAllSuperheros();
+            List<Power> powers = powerDao.getAllPowers();
+            model.addAttribute("allSuperheroes", allSuperheroes);
+            model.addAttribute("powers", powers);
+            model.addAttribute("errors", violations);
+            return "superheroes";
+        }
+
     }
 
     @GetMapping("superheroDetails")
@@ -129,12 +153,14 @@ public class SuperheroController {
         List<Power> powers = powerDao.getAllPowers();
         model.addAttribute("superhero", superhero);
         model.addAttribute("powers", powers);
+        violationsEdit.clear();
+        model.addAttribute("errors", violationsEdit);
 
         return "superheroEdit";
     }
 
     @PostMapping("superheroEdit")
-    public String performSuperheroEdit(HttpServletRequest request, @RequestParam(value = "action", required = true) String action, @RequestParam("file") MultipartFile file) {
+    public String performSuperheroEdit(Model model, HttpServletRequest request, @RequestParam(value = "action", required = true) String action, @RequestParam("file") MultipartFile file) {
         if (action.equals("cancel")) {
             return "redirect:/superheroes";
         }
@@ -156,8 +182,19 @@ public class SuperheroController {
             superhero.setPhotoFileName(imageDao.updateImage(file, superhero.getPhotoFileName(), SUPERHERO_UPLOAD_DIRECTORY));
         }
 
-        superheroDao.updateSuperhero(superhero);
+        Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
+        violationsEdit = validate.validate(superhero);
 
-        return "redirect:/superheroDetails?id=" + superhero.getSuperheroId();
+        if (violationsEdit.isEmpty()) {
+            superheroDao.updateSuperhero(superhero);
+            return "redirect:/superheroDetails?id=" + superhero.getSuperheroId();
+        } else {
+            model.addAttribute("superhero", superhero);
+            List<Power> powers = powerDao.getAllPowers();
+            model.addAttribute("powers", powers);
+            model.addAttribute("errors", violationsEdit);
+            return "superheroEdit";
+        }
+
     }
 }
